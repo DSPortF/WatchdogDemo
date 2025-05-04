3e706c77ff53e3254cfa47fb5bf6b101c9e99dd1
David S <63236987+DSPortF@users.noreply.github.com>
2025-05-04 10:34:33 +0100

    Start with the .NET Joke Service

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 # WatchdogDemo
 
 A demo watchdog that reboots the computer under certain failure scenarios.
+
+1. Start with the .NET Joke Service from https://learn.microsoft.com/en-us/dotnet/core/extensions/windows-service
+and confirm this all works OK.
+
+Create the service in an elevated command prompt or PowerShell:
+sc.exe create ".NET Joke Service" binpath= C:\_Dave\Dev\_Publish\WatchdogService\WatchdogService.exe"
+
+Other useful commands:
+sc.exe start ".NET Joke Service"
+sc.exe stop ".NET Joke Service"
+
+To find the jokes, look in Event Viewer - Windows Logs - Application
+
--- WatchdogService/Program.cs
+++ WatchdogService/Program.cs
@@ -1,12 +1,24 @@
+using App.WindowsService;
+using Microsoft.Extensions.Logging.Configuration;
+using Microsoft.Extensions.Logging.EventLog;
+
 namespace WatchdogService
 {
     public class Program
     {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Interoperability", "CA1416:Validate platform compatibility", Justification = "Application is Windows-only")]
         public static void Main(string[] args)
         {
             var builder = Host.CreateApplicationBuilder(args);
-            builder.Services.AddHostedService<Worker>();
+            builder.Services.AddWindowsService(options =>
+            {
+                options.ServiceName = ".NET Joke Service";
+            });
 
+            LoggerProviderOptions.RegisterProviderOptions<EventLogSettings, EventLogLoggerProvider>(builder.Services);
+
+            builder.Services.AddSingleton<JokeService>();
+            builder.Services.AddHostedService<WindowsBackgroundService>();
             var host = builder.Build();
             host.Run();
         }
